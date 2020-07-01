62c7ff4cb4ae7309e871f8debcd09ee6b32a548c
Sébastien BEAU <sebastien.beau@akretion.com>
2020-07-01 10:43:13 +0200

    [IMP] add http adapter

--- storage_backend/components/__init__.py
+++ storage_backend/components/__init__.py
@@ -2,3 +2,4 @@
 
 from . import base_adapter
 from . import filesystem_adapter
+from . import http_adapter
--- storage_backend/models/storage_backend.py
+++ storage_backend/models/storage_backend.py
@@ -18,7 +18,10 @@ class StorageBackend(models.Model):
 
     name = fields.Char(required=True)
     backend_type = fields.Selection(
-        selection=[("filesystem", "Filesystem")], required=True
+        selection=[
+            ("filesystem", "Filesystem"),
+            ("http", "Http"),
+            ], required=True
     )
     directory_path = fields.Char(
         sparse="data", help="Relative path to the directory to store the file"
